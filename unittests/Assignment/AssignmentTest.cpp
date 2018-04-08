63af456da9adafb5c92ba676cacf9db889597fde
Michael Simacek <msimacek@redhat.com>
2018-04-09 01:57:40 +0200

    Unit test for compact assignment

--- unittests/Assignment/AssignmentTest.cpp
+++ unittests/Assignment/AssignmentTest.cpp
@@ -35,3 +35,55 @@ TEST(AssignmentTest, FoldNotOptimized)
   ASSERT_TRUE(asConstant != NULL);
   ASSERT_EQ(asConstant->getZExtValue(), (unsigned) 128);
 }
+
+TEST(AssignmentTest, CompactModel)
+{
+  ArrayCache ac;
+  const Array* array = ac.CreateArray("simple_array", /*size=*/ 1);
+  Assignment::map_bindings_ty values;
+  MapArrayModel &model = values[array];
+  model.add(4, 8);
+  model.add(5, 16);
+  model.add(7, 17);
+  model.add(320000, 32);
+  Assignment a(values);
+  ASSERT_EQ(a.getValue(array, 0), 0);
+  ASSERT_EQ(a.getValue(array, 4), 8);
+  ASSERT_EQ(a.getValue(array, 5), 16);
+  ASSERT_EQ(a.getValue(array, 6), 0);
+  ASSERT_EQ(a.getValue(array, 7), 17);
+  ASSERT_EQ(a.getValue(array, 100), 0);
+  ASSERT_EQ(a.getValue(array, 320000), 32);
+  CompactArrayModel& compactModel = a.bindings[array];
+  MapArrayModel(a.bindings[array]).toCompact(compactModel);
+  ASSERT_EQ(a.getValue(array, 0), 0);
+  ASSERT_EQ(a.getValue(array, 4), 8);
+  ASSERT_EQ(a.getValue(array, 5), 16);
+  ASSERT_EQ(a.getValue(array, 6), 0);
+  ASSERT_EQ(a.getValue(array, 7), 17);
+  ASSERT_EQ(a.getValue(array, 100), 0);
+  ASSERT_EQ(a.getValue(array, 320000), 32);
+}
+
+TEST(AssignmentTest, CompactModelAsVector)
+{
+  ArrayCache ac;
+  const Array* array = ac.CreateArray("simple_array", /*size=*/ 1);
+  Assignment::map_bindings_ty values;
+  MapArrayModel &model = values[array];
+  model.add(4, 8);
+  model.add(5, 16);
+  model.add(7, 17);
+  model.add(32, 32);
+  Assignment a(values);
+  std::vector<uint8_t> vec = a.bindings[array].asVector();
+  ASSERT_EQ(vec.size(), 33u);
+  ASSERT_EQ(vec[0], 0);
+  ASSERT_EQ(vec[1], 0);
+  ASSERT_EQ(vec[4], 8);
+  ASSERT_EQ(vec[5], 16);
+  ASSERT_EQ(vec[6], 0);
+  ASSERT_EQ(vec[7], 17);
+  ASSERT_EQ(vec[31], 0);
+  ASSERT_EQ(vec[32], 32);
+}
