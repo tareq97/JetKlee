351cc23227e698d01e14f7d4c62b279620bdfd80
Marek Chalupa <xchalup4@fi.muni.cz>
2019-02-13 19:52:31 +0100

    CexCachingSolver: pass const ref to shared_ptr where possible
    In some methods it is better to use const reference instead of
    copying shared pointer.

--- lib/Solver/CexCachingSolver.cpp
+++ lib/Solver/CexCachingSolver.cpp
@@ -107,11 +107,11 @@ public:
 ///
 
 struct NullAssignment {
-  bool operator()(std::shared_ptr<const Assignment> a) const { return !a; }
+  bool operator()(const std::shared_ptr<const Assignment>& a) const { return !a; }
 };
 
 struct NonNullAssignment {
-  bool operator()(std::shared_ptr<const Assignment> a) const { return a!=0; }
+  bool operator()(const std::shared_ptr<const Assignment>& a) const { return a!=0; }
 };
 
 struct NullOrSatisfyingAssignment {
@@ -119,7 +119,7 @@ struct NullOrSatisfyingAssignment {
   
   NullOrSatisfyingAssignment(KeyType &_key) : key(_key) {}
 
-  bool operator()(std::shared_ptr<const Assignment> a) const {
+  bool operator()(const std::shared_ptr<const Assignment>& a) const {
     return !a || a->satisfies(key.begin(), key.end()); 
   }
 };
