a7e6112929618e6faf4c0fe0e90ba55a90507476
Marek Chalupa <mchqwerty@gmail.com>
2019-10-27 09:45:26 +0100

    SpecialFunctionHanlder: remove handling aliases
    Some mistake during rebasing...

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -314,20 +314,6 @@ void SpecialFunctionHandler::handleSilentExit(ExecutionState &state,
   executor.terminateState(state);
 }
 
-void SpecialFunctionHandler::handleAliasFunction(ExecutionState &state,
-						 KInstruction *target,
-						 const std::vector<Cell> &arguments) {
-  assert(arguments.size()==2 && 
-         "invalid number of arguments to klee_alias_function");
-  std::string old_fn = readStringAtAddress(state, arguments[0]);
-  std::string new_fn = readStringAtAddress(state, arguments[1]);
-  KLEE_DEBUG_WITH_TYPE("alias_handling", llvm::errs() << "Replacing " << old_fn
-                                           << "() with " << new_fn << "()\n");
-  if (old_fn == new_fn)
-    state.removeFnAlias(old_fn);
-  else state.addFnAlias(old_fn, new_fn);
-}
-
 void SpecialFunctionHandler::handleAssert(ExecutionState &state,
                                           KInstruction *target,
                                           const std::vector<Cell> &arguments) {
